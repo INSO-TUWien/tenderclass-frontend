5e76ed6b919c122bea17a900b3546b1ad543de7e
lukas <lukas.arnhold@hotmail.com>
2020-10-18 20:29:29 +0200

    update dtos

--- src/app/dtos/tender.ts
+++ src/app/dtos/tender.ts
@@ -4,7 +4,9 @@ export class Tender {
   constructor(
     public id: string,
     public cpvs: string[],
-    public languageentities: LanguageEntity[]
+    public languageentities: LanguageEntity[],
+    original_lang: string,
+    original_lang_entity: LanguageEntity
     ) {
   }
 }
